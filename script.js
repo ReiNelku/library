4cd8de05a9cf0a254f2abc36fb37644d1a996e46
Rei Nelku <85695388+ReiNelku@users.noreply.github.com>
2024-05-27 17:15:33 +0200

    feat: Display an indicator if no books are added

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ function Book(author, title, pages, isRead) {
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
+
+const main = document.querySelector("main");
+
+function displayBooks() {
+  if (myLibrary.length === 0) {
+    const noBooks = document.createElement("h3");
+    noBooks.textContent = "No Books Added Yet!";
+    noBooks.classList.add("no-books");
+
+    main.appendChild(noBooks);
+    return;
+  }
+}
+
+displayBooks();
