2a06bfd5afac764f3c7c9db94ecd8a1499b7d0b1
Rei Nelku <85695388+ReiNelku@users.noreply.github.com>
2024-05-27 16:59:23 +0200

    feat: Create book constructor

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(author, title, pages, isRead) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
